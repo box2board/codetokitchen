adc07eafd134e9edb556e7f284ad8def6573fb05
box2board <ashdchristie@gmail.com>
2026-01-16 15:37:31 -0500

    Add global Monetag loader

---
+++ assets/monetag.js
@@ -0,0 +1,27 @@
+(() => {
+  if (typeof document === 'undefined') {
+    return;
+  }
+
+  const existingScript = document.querySelector('script[src*="nap5k.com/tag.min.js"]');
+  if (existingScript) {
+    return;
+  }
+
+  const target = document.body || document.documentElement;
+  if (!target) {
+    return;
+  }
+
+  window.monetag = window.monetag || {};
+  window.monetag.config = {
+    avoidInterstitials: true,
+    avoidPopunders: true,
+  };
+
+  const script = document.createElement('script');
+  script.dataset.zone = '10474979';
+  script.src = 'https://nap5k.com/tag.min.js';
+  script.async = true;
+  target.appendChild(script);
+})();
